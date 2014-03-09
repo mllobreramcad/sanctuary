d5164ff097e14d1e3f896904cba96c445fd48c56
Mark Llobrera <mark@bluecadet.com>
2014-03-09 00:04:34 -0500

    parallax

--- functions.php
+++ functions.php
@@ -97,10 +97,12 @@ function sanctuary_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-  wp_enqueue_script( 'jquery' );
-  wp_enqueue_script( 'scrollorama', get_template_directory_uri() . '/js/jquery.scrollorama.js', array('jquery'), '20140308', true );
-  wp_enqueue_script( 'scrolldeck', get_template_directory_uri() . '/js/jquery.scrolldeck.js', array('jquery'), '20140308', true );
-  wp_enqueue_script( 'jquery-easing', get_template_directory_uri() . '/js/jquery.easing.1.3.js', array('jquery'), '20140308', true );
+  wp_enqueue_script( 'jquery');
+  wp_enqueue_script( 'scrollorama', get_template_directory_uri() . '/js/jquery.scrollorama.js', array('jquery'), '', true );
+  wp_enqueue_script( 'scrolldeck', get_template_directory_uri() . '/js/jquery.scrolldeck.js', array('jquery'), '', true );
+  wp_enqueue_script( 'jquery-easing', get_template_directory_uri() . '/js/jquery.easing.1.3.js', array('jquery'), '', true );
+  wp_enqueue_script( 'parallax', get_template_directory_uri() . '/js/jquery.parallax-1.1.js', array('jquery'), '', true );
+  wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array('jquery'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'sanctuary_scripts' );
 

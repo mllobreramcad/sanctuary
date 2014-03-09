16adfc972191ddd27974f9feebe596c1e45450a2
Mark Llobrera <mark@bluecadet.com>
2014-03-09 00:09:14 -0500

    background assigned using jquery

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+  $('.group').each(function(){
+    var backgroundStyle = "url("+$(this).attr('data-background')+")";
+    console.log('backgroundStyle', backgroundStyle, $(this));
+    $(this).css("background-image", backgroundStyle);
+  });
+});

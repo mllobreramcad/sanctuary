e5d85c268f541970a7b41f9c600765c1b3d8ab5b
Mark Llobrera <mark@bluecadet.com>
2014-03-09 00:14:20 -0500

    final parallax

--- js/main.js
+++ js/main.js
@@ -4,5 +4,11 @@ jQuery(document).ready(function($) {
     console.log('backgroundStyle', backgroundStyle, $(this));
     $(this).css("background-image", backgroundStyle);
   });
-
+  
+  $('.group1').parallax("50%", 0, 0.1, true);
+  $('.group2').parallax("50%", 0, 0.1, true);
+  $('.group3').parallax("50%", 0, 0.1, true);
+  $('.group4').parallax("50%", 0, 0.1, true);
+  $('.group5').parallax("50%", 0, 0.1, true);
+  
 });
